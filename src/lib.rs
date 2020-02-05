3d644dddc4a2e8ca34217ca2576886776e45904c
mikelma <mikelma7@gmail.com>
2020-02-05 22:29:12 +0100

    Added USR command support and testing

--- src/lib.rs
+++ src/lib.rs
@@ -36,52 +36,65 @@ pub enum CommandCode {
     Err = 1,
     Get = 2,
     Msg = 3,
+    Usr = 4, // User log in command
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Command {
     Ok,
-    Err(String),
+    Err(String), // text (error)
     Get,
-    Msg(String, String, String),
+    Msg(String, String, String), // sender, receiver, text
+    Usr(String, String), // sender (username), text (password)
 }
 
 pub struct RawMessage;
 
 impl RawMessage {
+    
+    /// Parses the text segment of a given byte buffer into a string.
+    /// It cares about the text length parameter given in the TXT_LEN 
+    /// segment of the message.
+    fn parse_text(raw: &[u8]) -> String {
+        // Get the length of the message text
+        let mut range = [0u8;2];
+        range[0] = raw[TXT_LEN.start];
+        range[1] = raw[TXT_LEN.end];
+        let n: usize = u16::from_ne_bytes(range) as usize;
+        // Convert txt to string
+        let text = String::from_utf8_lossy(&raw[TXT_BYTES][..n]);
+        text.to_string()
+    }
 
-    pub fn from_raw(raw: &[u8]) -> Result<Command, io::Error> {
+pub fn from_raw(raw: &[u8]) -> Result<Command, io::Error> {
         // Check the first byte for command code
         match FromPrimitive::from_u8(raw[0]) {
             Some(CommandCode::Ok) => Ok(Command::Ok),
             Some(CommandCode::Err) => {
-                // Get error message length
-                let mut range = [0u8;2];
-                range[0] = raw[TXT_LEN.start];
-                range[1] = raw[TXT_LEN.end];
-                let n: usize = u16::from_ne_bytes(range) as usize;
-
-                // Code the error into an utf-8 string
-                let error = String::from_utf8_lossy(&raw[TXT_BYTES][..n]);
-                Ok(Command::Err(error.to_string()))
+                // Get the error message from the text segment
+                Ok(Command::Err(RawMessage::parse_text(&raw)))
             },
             Some(CommandCode::Get) => Ok(Command::Get),
             Some(CommandCode::Msg) => {
+                // Get sender name
                 let n = raw[SENDER_LEN] as usize;
                 let sender = String::from_utf8_lossy(&raw[SENDER_BYTES][..n]);
+                // Get receiver name
                 let n = raw[RECV_LEN] as usize;
                 let recv = String::from_utf8_lossy(&raw[RECV_BYTES][..n]);
-                
-                // Get the length of the message text
-                let mut range = [0u8;2];
-                range[0] = raw[TXT_LEN.start];
-                range[1] = raw[TXT_LEN.end];
-                let n: usize = u16::from_ne_bytes(range) as usize;
+                // Parse the message text into a string 
+                let text = RawMessage::parse_text(&raw);
 
-                // Convert txt to string
-                let text = String::from_utf8_lossy(&raw[TXT_BYTES][..n]);
+                Ok(Command::Msg(sender.to_string(), recv.to_string(), text))
+            },
+            Some(CommandCode::Usr) => {
+                // Get sender's username
+                let n = raw[SENDER_LEN] as usize;
+                let username = String::from_utf8_lossy(&raw[SENDER_BYTES][..n]);
+                // Get password from the text segment 
+                let password = RawMessage::parse_text(&raw);
 
-                Ok(Command::Msg(sender.to_string(), recv.to_string(), text.to_string()))
+                Ok(Command::Usr(username.to_string(), password))
             },
             None => Err(io::Error::new(io::ErrorKind::InvalidData, 
                                        format!("Incorrect command byte: {}", raw[0]))),
@@ -118,17 +131,11 @@ impl RawMessage {
             Command::Err(err) => {
                 // Set command code
                 buffer[0] = CommandCode::Err as u8;
-
                 // Set the error message length bytes
-                let n = match u16::try_from(err.len()) {
-                    Ok(n) => n.to_ne_bytes(),
-                    Err(_) => return Err(io::Error::new(io::ErrorKind::InvalidInput, 
-                                                  "Error message length exceded"))
-                };
-
+                let err = err.as_bytes();
+                let n = RawMessage::compute_text_length(&err)?;
                 RawMessage::put(&mut buffer, &n, TXT_LEN)?;
                 // Append the error's bytes to the buffer's text section
-                let err = err.as_bytes();
                 RawMessage::put(&mut buffer, err, TXT_BYTES)?;
             },
             Command::Get => buffer[0] = CommandCode::Get as u8,
@@ -143,21 +150,42 @@ impl RawMessage {
                 let r = r.as_bytes();
                 buffer[RECV_LEN] = r.len() as u8;
                 RawMessage::put(&mut buffer, r, RECV_BYTES)?;
-
                 // Set the txt message length bytes
                 let t = t.as_bytes();
-                let n = match u16::try_from(t.len()) {
-                    Ok(n) => n.to_ne_bytes(),
-                    Err(_) => return Err(io::Error::new(io::ErrorKind::InvalidInput, 
-                                                  "Error message length exceded"))
-                };
+                let n = RawMessage::compute_text_length(&t)?;
                 RawMessage::put(&mut buffer, &n, TXT_LEN)?;
                 // Add the messgase's body
                 RawMessage::put(&mut buffer, t, TXT_BYTES)?;
             },
+            Command::Usr(username, password) => {
+                // Set USR command code
+                buffer[0] = CommandCode::Usr as u8;
+                // Set sender's username
+                let username = username.as_bytes();
+                buffer[SENDER_LEN] = username.len() as u8; // Set sender name size
+                RawMessage::put(&mut buffer, username, SENDER_BYTES)?;
+                // Set the password's buffer length
+                let password = password.as_bytes(); 
+                let length = RawMessage::compute_text_length(&password)?;
+                RawMessage::put(&mut buffer, &length, TXT_LEN)?;
+                // Set password in the text segment of the message
+                RawMessage::put(&mut buffer, password, TXT_BYTES)?;
+            },
         }
 
         Ok(buffer)
+    }
+    
+    /// Return's a 2Byte representation of the length of a given byte buffer.
+    /// # Errors:
+    /// Returns an InvalidInput error if the length of the buffer can 
+    /// not be represented in two bytes.
+    fn compute_text_length(buffer: &[u8]) -> Result<[u8;2], io::Error> {
+        match u16::try_from(buffer.len()) {
+            Ok(n) => Ok(n.to_ne_bytes()),
+            Err(_) => return Err(io::Error::new(io::ErrorKind::InvalidInput, 
+                                        "Error message length exceded"))
+        }
     }
 }
 
@@ -203,5 +231,17 @@ fn test_msg() {
     let mesg = RawMessage::to_raw(&command).unwrap();
     let recovered = RawMessage::from_raw(&mesg).unwrap();
     assert_eq!(mesg[0], 3);
+    assert_eq!(command, recovered);
+}
+
+#[test]
+fn test_usr() {
+    // Test error command
+    let command = Command::Usr("sender".to_string(),
+                               "The super secret password".to_string());
+
+    let mesg = RawMessage::to_raw(&command).unwrap();
+    let recovered = RawMessage::from_raw(&mesg).unwrap();
+    assert_eq!(mesg[0], 4);
     assert_eq!(command, recovered);
 }
